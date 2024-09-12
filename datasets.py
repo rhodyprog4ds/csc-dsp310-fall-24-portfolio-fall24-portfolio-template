a6efa0f0582336cbe8790a4dee7b82c500abddc3
Sarah M Brown <brownsarahm@uri.edu>
2024-09-12 15:11:20 -0400

    a2 template

---
+++ datasets.py
@@ -0,0 +1,10 @@
+
+dataset_list = [{'url': , # a dataset with nonnumerical data in at least one column
+     'name': ,
+     'load_func':},
+     {'url': , #any dataset you're interested in
+     'name': ,
+     'load_func':},
+     {'url': , #any dataset you're interested in
+     'name': ,
+     'load_func':}]
